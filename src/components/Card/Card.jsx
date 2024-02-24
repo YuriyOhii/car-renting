bf3314a881b859a490bb170520b646f8c60049b1
YuriyOhii <uaogiy@gmail.com>
2024-02-24 21:20:21 +0200

    card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,14 +4,30 @@ import { useState } from 'react';
 import { ExtraInfo } from '../ExtraInfo/ExtraInfo';
 import { customStyles } from './Card.styled';
 
-export const Card = () => {
+export const Card = ({ car }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const closeModal = () => setIsModalOpen(false);
   const openModal = () => setIsModalOpen(true);
 
   return (
     <>
-      <div>Card</div>
+      <button>favourite</button>
+      <img src={car.img} alt="Car photo" />
+      <div>
+        <h2>
+          {car.make}
+          {car.model}, {car.year}
+        </h2>
+        <p>{car.rentalPrice}</p>
+      </div>
+      <div>
+        <span>{car.adress}</span>
+        <span>{car.rentalCompany}</span>
+        <span>{car.type}</span>
+        <span>{car.make}</span>
+        <span>{car.id}</span>
+        <span>{car.functionalities[0]}</span>
+      </div>
       <button type="button" onClick={openModal}>
         Learn more
       </button>
@@ -21,7 +37,7 @@ export const Card = () => {
         style={customStyles}
         contentLabel="Extra Info Modal"
       >
-        <ExtraInfo />
+        <ExtraInfo car={car} />
       </Modal>
     </>
   );
