d9526f104bff62f7994b1ee19ba9c740b03afa43
YuriyOhii <uaogiy@gmail.com>
2024-02-25 12:44:08 +0200

    gallery component

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,13 +1,23 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
+import { fetchCars } from '../redux/cars/operations';
 import { Filters } from '../components/Filters/Filters';
 import { Gallery } from '../components/Gallery/Gallery';
+import { selectCars } from '../redux/cars/selectors';
 export default function CatalogPage() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCars());
+  }, [dispatch]);
+  const cars = useSelector(selectCars);
   return (
     <>
       <div>CatalogPage</div>
       <Link to={'/favourite'}>To look throw the favorites</Link>
       <Filters />
-      <Gallery />
+      <Gallery items={cars} />
     </>
   );
 }
